14a74e1335562161eedc0100c4d89c91212fbd12
macmzon <macmzon@163.com>
2018-11-27 14:22:44 +0800

    修改stylus css样式

--- src/components/startPage.vue
+++ src/components/startPage.vue
@@ -31,13 +31,13 @@ export default {
 }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  .hello {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+<style lang="stylus" scoped>
+  .hello
+    font-family: 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    text-align: center
+    color: #2c3e50
+    margin-top: 60px
+
 </style>
